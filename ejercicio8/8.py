afd61a23f4134b9e42c3663b43b62255794a4bba
Noelia Romina Ruiz <estefitrinidad@gmail.com>
2022-06-30 02:27:37 -0300

    ejercicio 8

---
+++ ejercicio8/8.py
@@ -0,0 +1,13 @@
+"""
+Escribí un programa que solicite al usuario el ingreso de dos palabras,
+las cuales se guardarán en dos variables distintas. A continuación,
+almacená en una variable la concatenación de la primera palabra, más un espacio,
+más la segunda palabra. Mostrá este resultado en pantalla. 
+"""
+
+palabra1 = input("ingresà la primer palabra: ")
+palabra2 = input("ingresà la segunda palabra: ")
+
+concatenacion = palabra1 + " " + palabra2
+
+print(concatenacion)
