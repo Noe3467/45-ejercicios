62f56b451edff1c01c3787c38f5741a7ecf48085
Noelia Romina Ruiz <estefitrinidad@gmail.com>
2022-07-06 00:40:38 -0300

    ejercicio 23

---
+++ ejercicio23/23.py
@@ -0,0 +1,11 @@
+"""
+Escribí un programa que le solicite al usuario un número entero y muestre todos
+los números correlativos entre el 1 y el número ingresado por el usuario. 
+"""
+
+i = 0
+number = int(input("ingresà un nùmero entero: "))
+
+while i < number:
+    i = i+1
+    print(i)
