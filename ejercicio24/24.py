59cf5d763671cdcb431c749b13b8849a047ab7f7
Noelia Romina Ruiz <estefitrinidad@gmail.com>
2022-07-06 00:42:15 -0300

    ejercicio 24

---
+++ ejercicio24/24.py
@@ -0,0 +1,11 @@
+"""
+Escribí un programa que muestre la sumatoria de todos los números entre el 0 y el 100. 
+"""
+i = 0
+suma = 0
+
+for i in range(100):
+    i = i +1
+    suma = suma + i
+
+print(suma)
